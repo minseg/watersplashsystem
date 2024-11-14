c0f320819a5ae8dd30a833b7b5d9efc6c56dbcd5
minseg <artssam7602@naver.com>
2024-11-15 00:30:02 +0900

    예전 OOP 방식을 새 방식으로 리팩토링

--- Particle.js
+++ Particle.js
@@ -2,10 +2,11 @@ class Particle {
     constructor(x, y) {
       this.position = createVector(x, y);
       this.velocity = createVector(random(-1, 1), random(-1, 0));
-      this.acceleration = createVector(0, 0.05);
+      this.acceleration = createVector(0, 0); // 중력은 외부에서 적용
       this.lifespan = 255;
     }
   
+    // 외부에서 힘을 적용할 수 있도록 함수 분리
     applyForce(force) {
       this.acceleration.add(force);
     }
@@ -13,7 +14,7 @@ class Particle {
     update() {
       this.velocity.add(this.acceleration);
       this.position.add(this.velocity);
-      this.acceleration.mult(0);
+      this.acceleration.mult(0);  // 가속도를 리셋
       this.lifespan -= 2;
     }
   
--- ParticleSystem.js
+++ ParticleSystem.js
@@ -2,6 +2,8 @@ class ParticleSystem {
     constructor(x, y) {
       this.origin = createVector(x, y);
       this.particles = [];
+      this.gravity = createVector(0, 0.05); // 중력
+      this.wind = createVector(0.1, 0);  // 바람
     }
   
     addParticle() {
@@ -11,11 +13,18 @@ class ParticleSystem {
     run() {
       for (let i = this.particles.length - 1; i >= 0; i--) {
         let p = this.particles[i];
+        
+        // 중력과 바람을 각 파티클에 적용
+        p.applyForce(this.gravity);
+        p.applyForce(this.wind);
+        
         p.update();
         p.display();
+        
         if (p.isDead()) {
           this.particles.splice(i, 1);
         }
       }
     }
   }
+  
